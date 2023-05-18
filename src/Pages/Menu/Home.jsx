39a9eb4c6d8fe26a5656b98cfe98b7afefa5cfe6
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-05-18 14:05:00 -0500

    Creacion de main sin funciones en los botones y solo responsibe en pc

--- src/Pages/Menu/Home.jsx
+++ src/Pages/Menu/Home.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import './Home.scss';
 import { Header } from '../../components/Navegacion/Header';
+import image from '../../../public/Fondo.svg';
+import LogoMain from '../../../public/logoMain.svg';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
   return (
     <>
+      
+      <div className="wallpaper">
+        <img src={image} alt="Fondo" />
+        
+      </div>
+
       <Header />
-      <h1>Inicio</h1>
+
+      <div className="main">
+        <div className="containerLeft">
+          <h1>ANKA PHOTOGRAPY</h1>  
+          <p>Anka Photograpy es un servicio de fotografia totalmente profesional, confiable y 
+            economico. Contratanos ya para tus eventos.</p>
+
+            <div className='Button'>
+              <Link className='btnSessions'>Pide tu sesión ya</Link>
+              <Link className='btnIndormation'>Ver más</Link>
+            </div>
+        </div>
+        <div className="containerRight">
+          <img src={LogoMain} alt="Logo" />
+        </div>
+      </div>
+      
+
+
     </>
   );
 }
