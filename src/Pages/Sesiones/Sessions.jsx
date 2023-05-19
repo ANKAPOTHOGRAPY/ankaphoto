a6c8e124876a8fdd8b44f0f11ce0e5c61dbbb6c1
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-05-18 19:56:53 -0500

    paginas de sesiones terminada

--- src/Pages/Sesiones/Sessions.jsx
+++ src/Pages/Sesiones/Sessions.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import { Header } from '../../components/Navegacion/Header';
+import '../Sesiones/Sessions.scss';
+
 
 function Sessions() {
     return(
         <>
+        <div className="containerSessions">
+            
             <Header />
-            <h1>Sesiones</h1>
+            <div className="sessions">
+                <div className="Item1">
+                    <p>Anderson</p>
+                    <img src="../../../public/img/image 3.png" alt="imagen de Anderson" />
+                </div>
+
+                <div className="Item2">
+                    <p>Mileidys</p>
+                    <img src="../../../public/img/image 5.png" alt="imagen de Anderson" />
+                </div>
+
+                <div className="Item3">
+                    <p>Maite</p>
+                    <img src="../../../public/img/image 6.png" alt="imagen de Anderson" />
+                </div>
+
+                <div className="Item4">
+                    <p>Kyanna</p>
+                    <img src="../../../public/img/image 4.png" alt="imagen de Anderson" />
+                </div>
+            </div>
+            
+
+        </div>
+        
         </>
         
     );
